715a63febb572e510c0625525b95821820084b48
Rolando Espinoza La fuente <darkrho@gmail.com>
2010-07-04 21:09:40 -0400

    allow to change locale on StringToDatetime processor Sign-Off: Rolando Espinoza La fuente

--- rho_blogs/processors.py
+++ rho_blogs/processors.py
@@ -1,9 +1,29 @@
+from __future__ import with_statement
+
 import datetime
+import locale
+
+from contextlib import contextmanager
+
+@contextmanager
+def use_locale(value, category=locale.LC_ALL):
+    original = locale.getlocale()
+    locale.setlocale(category, value)
+    yield
+    locale.setlocale(category, original)
 
 
 class StringToDatetime(object):
-    def __init__(self, format):
+    def __init__(self, format, locale=None):
         self.format = format
+        self.locale = locale
 
     def __call__(self, value):
-        return datetime.datetime.strptime(value, self.format)
+        # strptime only likes str but not unicode
+        value = value.encode('utf-8')
+        if self.locale is None:
+            return datetime.datetime.strptime(value, self.format)
+        else:
+            with use_locale(self.locale):
+                return datetime.datetime.strptime(value, self.format)
+
